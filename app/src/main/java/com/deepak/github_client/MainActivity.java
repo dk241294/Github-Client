f87adb79653fbf3e5ce5d588abd04bf33d08c3f8
deepak kumar <deepak24121994@gmail.com>
2019-10-03 19:08:07 +0530

    initial commit

---
+++ app/src/main/java/com/deepak/github_client/MainActivity.java
@@ -0,0 +1,14 @@
+package com.deepak.github_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.deepak.github_client.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    
+}
