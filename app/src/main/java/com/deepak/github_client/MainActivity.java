7fa5fa6548c2065a0a2bdf6840e133d878b00f02
deepak kumar <deepak24121994@gmail.com>
2019-10-04 10:07:40 +0530

    login fragment
    class and ui

--- app/src/main/java/com/deepak/github_client/MainActivity.java
+++ app/src/main/java/com/deepak/github_client/MainActivity.java
@@ -1,6 +1,7 @@
 package com.deepak.github_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -10,5 +11,19 @@ import com.deepak.github_client.R;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadLoginFragment();
+    }
+
+
+    public void loadLoginFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container, new LoginFragment(), LoginFragment.TAG);
+        ft.addToBackStack(null);
+        ft.commit();
+
+    }
 }
